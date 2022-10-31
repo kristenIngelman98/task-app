458f05ad2f2a14984d2a1ccbd99a393705f8c408
Kristen <kristeningelman@hotmail.com>
2022-10-31 12:33:28 -0700

    created resource creation endpoints

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,63 +1,12 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
+// const validator = require('validator');
 
 // connect to db w/ db url & db name
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true
 })
 
-// define models
-const User = mongoose.model('User', {
-    name: {
-        type: String, 
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot include "password"')
-            }
-        }
 
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
 
 // create instances of our models
 // create new user - instance of our model
@@ -69,10 +18,10 @@ const Task = mongoose.model('Task', {
 // })
 
 // create new task
-const task = new Task({
-    description: 'grocery shopping',
-    completed: true
-})
+// const task = new Task({
+//     description: 'grocery shopping',
+//     completed: true
+// })
 
 // save method doesnt take any params, just saves our data
 // me.save().then(() => {
@@ -81,8 +30,8 @@ const task = new Task({
 //     console.log('Error!', error)
 // })
 
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log('Oops!', error)
-})
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log('Oops!', error)
+// })
